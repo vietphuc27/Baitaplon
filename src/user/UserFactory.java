495e1c95999821be256cdec3bbd9f3f6f6eede12
Ten Cua Ong <dotrungnghia7@gmail.com>
2026-04-03 23:35:09 +0700

    them file UserFactory

---
+++ src/user/UserFactory.java
@@ -0,0 +1,26 @@
+public class UserFactory {
+    
+    private static UserFactory instance;
+
+    private UserFactory(){};
+
+    public static UserFactory getInstance(){
+        if (instance == null){
+            instance = new UserFactory();
+        }
+        return instance;
+    }
+
+    public User createUser(String type, String id, String username, String email, String password){
+        if (type.equalsIgnoreCase("Seller")){
+            return new Seller(id, username, email, password);
+        }
+        else if (type.equalsIgnoreCase("Bidder")){
+            return new Bidder(id, username, email, password);
+        }
+        else if (type.equalsIgnoreCase("Admin")){
+            return new Admin(id, username, email, password);
+        }
+        return null;
+    }
+}
