dd3f39ab01b5f7006ba67c3cda9429aafb724088
anhviet2101 <giangvietanh2101@gmail.com>
2026-04-19 00:32:29 +0700

    move LogInView and its css to resources.view

--- src/main/java/view/LoginView/Login/src/main/java/com/example/demo1/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo1;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(LoginApplication.class, args);
-    }
-}
