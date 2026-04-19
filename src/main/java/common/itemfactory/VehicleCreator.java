8acaa86b31902427eeb3e5ed6ea78591f980fe8d
Ten Cua Ong <dotrungnghia7@gmail.com>
2026-04-19 20:16:02 +0700

    sửa itemfactory theo ocp

---
+++ src/main/java/common/itemfactory/VehicleCreator.java
@@ -0,0 +1,18 @@
+package common.itemfactory;
+
+import common.models.item.*;
+
+public class VehicleCreator extends ItemCreator {
+    protected String type = "vehicle";
+
+    static {ItemFactory.addCreator(new VehicleCreator());}
+
+    @Override
+    public Item create(String id, String name, String description, double startingPrice, String sellerId, Object bonus){
+            return new Vehicle(id, name, description, startingPrice, sellerId, (int) bonus);
+    }
+    @Override
+    public String getType(){
+        return type;
+    }
+}
